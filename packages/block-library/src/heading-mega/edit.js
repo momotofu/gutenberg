0934a346e8f51f9110087eff04394f837e1f0829
tellthemachines <isabel@tellthemachines.com>
2020-01-06 17:43:35 +1100

    Added RichText component and deprecation.

--- packages/block-library/src/heading-mega/edit.js
+++ packages/block-library/src/heading-mega/edit.js
@@ -1,28 +1,64 @@
 /**
  * External dependencies
  */
-//import classnames from 'classnames';
+import classnames from 'classnames';
 
 /**
  * Internal dependencies
  */
-//import HeadingToolbar from './heading-toolbar';
+import HeadingToolbar from '../heading/heading-toolbar';
 
 /**
  * WordPress dependencies
  */
-//import { __ } from '@wordpress/i18n';
+import { __ } from '@wordpress/i18n';
 //import { PanelBody } from '@wordpress/components';
-//import { createBlock } from '@wordpress/blocks';
-//import {
-//AlignmentToolbar,
-//BlockControls,
-//InspectorControls,
-//RichText,
-//__experimentalUseColors,
-//} from '@wordpress/block-editor';
+import { createBlock } from '@wordpress/blocks';
+import {
+	AlignmentToolbar,
+	BlockControls,
+	// InspectorControls,
+	RichText,
+	// __experimentalUseColors,
+} from '@wordpress/block-editor';
 
-function HeadingEdit() {
+function HeadingEdit( { attributes, className, mergeBlocks, onReplace, setAttributes } ) {
+	const { align, content, level, placeholder } = attributes;
+	const tagName = 'h' + level;
+
+	return (
+		<>
+			<BlockControls>
+				<HeadingToolbar minLevel={ 2 } maxLevel={ 5 } selectedLevel={ level } onChange={ ( newLevel ) => setAttributes( { level: newLevel } ) } />
+				<AlignmentToolbar value={ align } onChange={ ( nextAlign ) => {
+					setAttributes( { align: nextAlign } );
+				} } />
+			</BlockControls>
+			<RichText
+				identifier="content"
+				tagName={ tagName }
+				value={ content }
+				onChange={ ( value ) => setAttributes( { content: value } ) }
+				onMerge={ mergeBlocks }
+				onSplit={ ( value ) => {
+					if ( ! value ) {
+						return createBlock( 'core/paragraph' );
+					}
+
+					return createBlock( 'core/heading', {
+						...attributes,
+						content: value,
+					} );
+				} }
+				onReplace={ onReplace }
+				onRemove={ () => onReplace( [] ) }
+				className={ classnames( className, {
+					[ `has-text-align-${ align }` ]: align,
+				} ) }
+				placeholder={ placeholder || __( 'Write heading…' ) }
+			/>
+		</>
+	);
 }
 
 export default HeadingEdit;
